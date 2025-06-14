4effd87f5d79dca82e05db6157d46a381c5e2eb2
Aayush Samal <aayushxtech@gmail.com>
2025-06-14 15:57:57 +0530

    refactor: clean up admin, models, urls, and views by removing commented code and improving structure

--- backend/parcels/admin.py
+++ backend/parcels/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from .models import Parcel
 
 
 admin.site.register(Parcel)
->>>>>>> 26bfea981fd2d7e08a7f9b7b00c97915a204975d
